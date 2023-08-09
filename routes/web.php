75ed5967186c3a721fe0ec85b889f307e18521f9
Paul <pablovanduo@gmail.com>
2023-08-09 09:49:41 +0300

    Add user registration, login

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\ListingController;
 use App\Models;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 // All listings
 Route::get('/', [ListingController::class, 'index']);
@@ -26,3 +27,18 @@ Route::delete('/listings/{listing}', [ListingController::class, 'delete']);
 
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show register
+Route::get('/register', [UserController::class, 'register']);
+
+// Create new user
+Route::post('/users', [UserController::class, 'store']);
+
+// Logout user
+Route::post('/logout', [UserController::class, 'logout']);
+
+// Show login form
+Route::get('/login', [UserController::class, 'login']);
+
+// Login user
+Route::post('/users/auth', [UserController::class, 'auth']);
